1aca3b2100110a2c4a9908b2ed0d35001648b042
emmacc02 <emmacaitlincraig@gmail.com>
2026-05-30 13:14:24 +0200

    Update library_screen.py

--- library_screen.py
+++ library_screen.py
@@ -28,11 +28,13 @@ with open("scFv_Cetuximab.fasta", "w") as file:
 # Mask CDR3 Regions for Mutations
 fasta_file = "scFv_Cetuximab.fasta"
 
-motif_CDR3_L = "QQNNNWPTT"
+motif_CDR3_L_target = "NNNW"
 
-motif_CDR3_H = "ALTYYDYEFAY"
+motif_CDR3_H_target = "TYYDY"
 
-def get_CDR3_positions(fasta_file, motif):
+motif_CDR2_H = "WSGGN"
+
+def get_CDR_positions(fasta_file, motif):
     for record in SeqIO.parse(fasta_file, "fasta"):
         seq = str(record.seq)
         start = seq.find(motif)
@@ -47,14 +49,15 @@ def get_CDR3_positions(fasta_file, motif):
 
         return start, end
 
-CDR3_L_pos = get_CDR3_positions(fasta_file, motif_CDR3_L)
-CDR3_H_pos = get_CDR3_positions(fasta_file, motif_CDR3_H)
+CDR3_L_pos = get_CDR_positions(fasta_file, motif_CDR3_L_target)
+CDR3_H_pos = get_CDR_positions(fasta_file, motif_CDR3_H_target)
+CDR2_H_pos = get_CDR_positions(fasta_file, motif_CDR2_H)
 
-CDR3_ranges = {
-    "VH": CDR3_H_pos,
+CDR_ranges = {
+    "VH_CDR3": CDR3_H_pos,
+    "VH_CDR2": CDR2_H_pos,
     "VL": CDR3_L_pos
 }
-
 
 #---------------------------------------------------------------------------------------------------------------------------------------
 # MASK FOR MUTATIONS 
@@ -68,19 +71,21 @@ def mask_sequence(seq, start, end, mask_char="X"):
 
 
 
-def mask_fasta(input_fasta, output_fasta, cdr3_ranges):
+def mask_fasta(input_fasta, output_fasta, cdr_ranges):
     masked_records = []
 
     for record in SeqIO.parse(input_fasta, "fasta"):
         seq = str(record.seq)
 
-        vh_start, vh_end = cdr3_ranges["VH"]
-        vl_start, vl_end = cdr3_ranges["VL"]
+        vh_cdr3_start, vh_cdr3_end = CDR_ranges["VH_CDR3"]
+        vh_cdr2_start, vh_cdr2_end = CDR_ranges["VH_CDR2"]
+        vl_start, vl_end = CDR_ranges["VL"]
 
-        seq = mask_sequence(seq, vh_start, vh_end)
+        seq = mask_sequence(seq, vh_cdr3_start, vh_cdr3_end)
+        seq = mask_sequence(seq, vh_cdr2_start, vh_cdr2_end)
         seq = mask_sequence(seq, vl_start, vl_end)
 
-        record.seq = Seq(seq)   # ✅ FIX HERE
+        record.seq = Seq(seq)   
 
         masked_records.append(record)
 
@@ -88,6 +93,6 @@ def mask_fasta(input_fasta, output_fasta, cdr3_ranges):
 
 mask_fasta(
     "scFv_Cetuximab.fasta",
-    "scFv_Cetuximab_CDR3_masked.fasta",
-    CDR3_ranges
+    "scFv_Cetuximab_masked.fasta",
+    CDR_ranges
 )
